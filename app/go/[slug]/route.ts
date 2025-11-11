a3bdb15b42fdb88a72a72e1c93f83b64a1a46f35
ReserveBTC <reservebtcproof@gmail.com>
2025-11-11 14:08:44 +0700

    Update affiliate links: Binance, OKX, AsterDEX, Hibachi

--- app/go/[slug]/route.ts
+++ app/go/[slug]/route.ts
@@ -2,13 +2,13 @@ import { NextResponse } from 'next/server';
 
 const AFFILIATE_LINKS: Record<string, string> = {
   // CEX
-  'binance': 'https://accounts.binance.com/register?ref=YOUR_REF_HERE',
+  'binance': 'https://www.binance.com/activity/referral-entry/CPA?ref=CPA_007U0H3CDE&utm_source=default',
   'bybit': 'https://www.bybit.com/invite?ref=YOUR_REF_HERE',
-  'okx': 'https://www.okx.com/join/YOUR_REF_HERE',
+  'okx': 'https://okx.ac/join/9121732',
   
   // DEX - Perpetual Futures
-  'asterdex': 'https://www.asterdex.com/en/referral/YOUR_ASTERDEX_REF',
-  'hibachi': 'https://hibachi.xyz/?ref=YOUR_HIBACHI_REF',
+  'asterdex': 'https://www.asterdex.com/en/referral/tlRYkq',
+  'hibachi': 'https://hibachi.xyz/r/hibachibothub',
   'lighter': 'https://lighter.xyz/?ref=YOUR_LIGHTER_REF',
   'hyperliquid': 'https://app.hyperliquid.xyz/join/YOUR_HYPERLIQUID_REF',
 };
